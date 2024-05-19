b9228ff8e89adb63b35a7ece0c11cee402da18b1
m <2528988582@qq.com>
2024-05-19 17:12:40 +0800

    chore: Update npm dependency to latest stable version

--- src/app.tsx
+++ src/app.tsx
@@ -5,18 +5,22 @@ import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
 import Nav from '~/components/Nav';
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query';
 
 export default function App() {
+  const client = new QueryClient();
   return (
-    <Router
-      root={props => (
-        <>
-          {/* <Nav /> */}
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <QueryClientProvider client={client}>
+      <Router
+        root={props => (
+          <>
+            {/* <Nav /> */}
+            <Suspense>{props.children}</Suspense>
+          </>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </QueryClientProvider>
   );
 }
