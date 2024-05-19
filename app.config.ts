e16f237ce4c5ab5a6e216d3dcbefc3358930667e
m <2528988582@qq.com>
2024-05-19 13:49:41 +0800

    chore: Update app.config.ts to use Deno server preset and remove unused code

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from '@solidjs/start/config';
 import UnoCSS from 'unocss/vite';
 
-const isNodejs =
-  typeof process !== 'undefined' &&
-  typeof process.versions !== null &&
-  typeof process.versions.node !== null;
-
 export default defineConfig({
   vite: {
     plugins: [UnoCSS()],
   },
   server: {
-    preset: isNodejs ? 'node-server' : 'deno-server',
+    preset: 'deno-server',
   },
 });
